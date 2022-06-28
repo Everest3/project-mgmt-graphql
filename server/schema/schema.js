5acfac4b2136306e9017550b8ab27cabb3cafede
Everest3 <restigjidia@outlook.com>
2022-06-29 00:36:58 +0200

    ok

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const {
   GraphQLEnumType,
 } = require('graphql');
 
+
 // Client Type
 const ClientType = new GraphQLObjectType({
   name: 'Client',
@@ -20,9 +21,48 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client:{
+      type: ClientType,
+      resolve(parent, args){
+        console.log({parent})
+        return clients.find(client => client.id === parent.clientId)
+      }
+    }
+  }),
+});
+
+
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects:{
+      type: new GraphQLList(ClientType),
+      resolve(parent, args){
+        return projects;
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find(client => client.id === args.id);
+      },
+    },
+    clients:{
+      type: new GraphQLList(ClientType),
+      resolve(parent, args){
+        return clients;
+      }
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
